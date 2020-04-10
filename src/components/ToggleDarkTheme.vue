a5d466bac8d732f6589000ec02cbf53741f01930
azukiazusa1 <cycleippei@gmail.com>
2020-04-10 19:09:50 +0900

    ダークテーマ

--- src/components/ToggleDarkTheme.vue
+++ src/components/ToggleDarkTheme.vue
@@ -9,14 +9,35 @@
 <script>
 export default {
   name: 'ToggleDarkTheme',
+  data() {
+    return {
+      strageKey: 'theme-mode'
+    }
+  },
   methods: {
     toggleDark() {
       this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      if (this.$vuetify.theme.dark) {
+        localStorage.setItem(this.strageKey,'dark')
+      } else {
+        localStorage.setItem(this.strageKey,'light')
+      }
+    }
+  },
+  created() {
+    const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+    const localTheme = localStorage.getItem(this.strageKey)
+    if  (localTheme === 'light') {
+      this.$vuetify.theme.dark = false
+    } else if (localTheme === 'dark'){
+      this.$vuetify.theme.dark = true
+    } else {
+      this.$vuetify.theme.dark = isDark
     }
   },
   computed: {
     getColor() {
-      return this.$vuetify.theme.dark ? 'deep-purple accent-4' : ''
+      return this.$vuetify.theme.dark ? 'yellow' : ''
     }
   }
 }
