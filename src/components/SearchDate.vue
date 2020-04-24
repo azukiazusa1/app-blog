9f1e9cbd714a4a0c64d7624164a94a7ad7b8ca1f
azukiazusa1 <cycleippei@gmail.com>
2020-04-24 20:00:16 +0900

    date picker 設置

---
+++ src/components/SearchDate.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-menu :offset-y=true>
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on">
+          <v-icon>fas fa-calendar-alt</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item>
+          <v-date-picker
+            v-model="picker"
+            type="month"
+            locale="zh-ja"
+          ></v-date-picker>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+</template>
+
+<script>
+export default {
+  name: 'search-date',
+  data() {
+    return {
+      picker: ''
+    }
+  }
+}
+</script>
