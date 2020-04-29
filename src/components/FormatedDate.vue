0dd5aa3e9c2578b73c5f9ff15a5d51b611146efa
azukiazusa1 <cycleippei@gmail.com>
2020-04-29 14:05:15 +0900

    日付表示コンポーネント

---
+++ src/components/FormatedDate.vue
@@ -0,0 +1,32 @@
+<template>
+  <span>
+    <v-btn icon><v-icon size="small">{{ icon }}</v-icon></v-btn>{{ createdTime }}
+  </span>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  name: 'formatedDate',
+  props: {
+    date: {
+      type: String,
+      required: true
+    },
+    format: {
+      type: String,
+      default: 'Y-MM-DD hh:mm:ss'
+    },
+    icon: {
+      type: String,
+      default: 'fas fa-calendar'
+    }
+  },
+  computed: {
+    createdTime () {
+      return moment(this.date).format(this.format)
+    }
+  }
+}
+</script>
