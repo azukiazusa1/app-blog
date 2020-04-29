508dc21203ccde6f3ad586d69e3e1a91939fe307
azukiazusa1 <cycleippei@gmail.com>
2020-04-29 14:04:50 +0900

    関連記事カード

---
+++ src/components/RelArticleCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card>
+    <v-img
+      :src="article.thumbnail"
+      alt="サムネイル"
+      height="200"
+    ></v-img>
+    <v-card-title>
+      <router-link :to="{ name: 'article-show', params: { id: article.id } }" class="text--primary">
+       {{ article.title }}
+      </router-link>
+    </v-card-title>
+    <v-card-text>
+      <formated-date :date="article.created.seconds * 1000"></formated-date>
+      <p>{{ article.overview }}</p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import FormatedDate from '@/components/FormatedDate'
+
+export default {
+  name: 'rel-article-card',
+  props: {
+    article: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    FormatedDate
+  }
+}
+</script>
